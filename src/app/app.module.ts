687856ee9440715d48e87235d837cdc2054e1313
Shun Miyaki <shunmiyaki@hotmail.com>
2018-07-05 14:22:32 +0900

    Update - You used the CLI to create a second HeroesComponent. - You displayed the HeroesComponent by adding it to the AppComponent shell. - You applied the UppercasePipe to format the name. - You used two-way data binding with the ngModel directive. - You learned about the AppModule. - You imported the FormsModule in the AppModule so that Angular would recognize and apply the ngModel directive. - You learned the importance of declaring components in the AppModule and appreciated that the CLI declared it for you.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
